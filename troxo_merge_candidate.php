1b15c8d3ec1edfe3c2065261b243c5d9feff9dc6
nemanj4t <nemanja.turanjanin@elfak.rs>
2019-03-05 14:05:44 +0100

    troxo_merge_candidate task implementation

--- troxo_merge_candidate.php
+++ troxo_merge_candidate.php
@@ -1,59 +1,114 @@
-<?php 
+<?php
 
+class Candidate
+{
+    public $name;
+    public $skills = array ();
+
+    public function __construct($name, array $skills)
+    {
+        $this->name = $name;
+        $this->skills = $skills;
+    }
+}
+
+abstract class Skills
+{
+    protected $skills = array ();
+
+    abstract function checkSkill(Candidate $you);
+}
+
+class BackendSkills extends Skills
+{
+    protected $skills = array ('php', '.net');
+
+    public function checkSkill(Candidate $you)
+    {
+        return !empty(array_intersect($this->skills, $you->skills));
+    }
+}
+
+class FrontendSkills extends Skills
+{
+    protected $skills = array ('html', 'css');
+
+    public function checkSkill(Candidate $you)
+    {
+        return array_intersect($this->skills, $you->skills) == $this->skills;
+    }
+}
+
+class FullStackSkills extends Skills
+{
+    protected $skills = array ('javascript');
+
+    public function checkSkill(Candidate $you)
+    {
+        return array_intersect($this->skills, $you->skills) == $this->skills;
+    }
+}
+
+class PersonalSkills extends Skills
+{
+    protected $skills = array ('english language', 'hungry for knowledge');
+
+    public function checkSkill(Candidate $you)
+    {
+        return array_intersect($this->skills, $you->skills) == $this->skills;
+    }
+}
 /**
-* @author Troxo Junior
-* @copyright Troxo d.o.o.
-* 
-* This file contains function for adding a new member to the Troxo team. 
-* Idea is for this function to evolve into a more complex module through iterative development process.
-* 
-* This file is still under development
-*
-*/
-
+ * @author Troxo Junior
+ * @copyright Troxo d.o.o.
+ *
+ * This file contains function for adding a new member to the Troxo team.
+ * Idea is for this function to evolve into a more complex module through iterative development process.
+ *
+ * This file is still under development
+ *
+ */
 /**
-* If apropriate conditions are met, these function will add a new member to the $troxoTeam array and will return true. 
-* Othervise it will return false.
-*
-* @param array $troxoTeam, Candidate $you
-* @return boolean
-*/
-merge(array $troxoTeam, Candidate $you) {
+ * If apropriate conditions are met, these function will add a new member to the $troxoTeam array and will return true.
+ * Othervise it will return false.
+ *
+ * @param array $troxoTeam, Candidate $you
+ * @return boolean
+ */
+function merge(& $troxoTeam, Candidate $you)
+{
+    $backendSkills = new BackendSkills();
+    $frontendSkills = new FrontendSkills();
+	$fullStackSkills = new FullStackSkills();
+	$personalSkills = new PersonalSkills();
 
-	$backendSkills = array (‘php’,’.net’); 
-	// @TODO skills could be a separate parent class while specific skills could be classes that inherit it
-	// checkSkill method could be implemented and inherited
+	if (!($conditionOne = $backendSkills->checkSkill($you))) 
+	{
+        return false;
+    }
 
-	$frontendSkills = array (‘html’, ‘css’); //array instancing backward compatible
+	if (!($conditionTwo = $frontendSkills->checkSkill($you))) 
+	{
+        return false;
+    }
 
-	$fullStackSkills = array (‘javascript’);
+    if (!($conditionThree = $fullStackSkills->checkSkill($you)))
+    {
+        return false;
+	}
+	
+	if (!($conditionFour = $personalSkills->checkSkill($you)))
+    {
+        return false;
+    }
 
-	$personalSkills = array (‘english language’,’hungry for knowledge’);
+    $troxoTeam[] = $you;
 
-If (in_array(‘php’, $you.skills) or in_array(‘.net’ $you.skills)) {
-	$conditionOne = true;
-} 
-
-If (array_intersect($frontendSkills, $you.skills) == $frontendSkills)) {
-	$conditionTwo = true;
+    return true;
 }
 
-//code duplication! These could be a separate function
-If (array_intersect($fullStackSkills, $you.skills) == $frontendSkills)) {
-	$conditionThree = true;
-}
-
-/* IT seems there is a more efficient way to go through these conditions */
-
-If ($conditionOne && $coditionTwo && $conditionThree) {
-
-	$troxoTeam[] = $you;
-
-	return true;
-
-} else {
-
-	return false;
-}
-
-?>
+// Example of usage
+//$troxoTeam = array();
+//$me = new Candidate('Nemanja', array ('php', 'html', 'css', 'javascript', 'english language', 'hungry for knowledge'));
+//
+//merge($troxoTeam, $me);
